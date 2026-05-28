d47a770bb9689c2b003107ba9fd49232c53ccf74
Alberto Lima <alberto.btlima@outlook.com>
2026-05-28 20:04:34 -0300

    add: index no repository

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -17,20 +17,29 @@ export class JsonPostRepository implements PostRepository {
     const jsonContent = await readFile(JSON_POSTS_FILE_PATH, "utf-8");
     const parsedJson = JSON.parse(jsonContent);
     const { posts } = parsedJson;
-
     return posts;
   }
 
   async findAll(): Promise<PostModel[]> {
     const posts = await this.readFromDisk();
-
     return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) throw new Error("Post não encontrado.");
+    return post;
   }
 }
 
-export const postRepository = new JsonPostRepository();
+// (async () => {
+//   const posts = await postRepository.findAll();
+//   console.log(posts);
 
-(async () => {
-  const posts = await postRepository.findAll();
-  console.log(posts);
-})();
+//   const post = await postRepository.findById(
+//     "99f8add4-7684-4c16-a316-616271db199e"
+//   );
+//   console.log(post);
+// })();
