4a088cfa7daa3d48ab4e929690ef2711657337b3
Alberto Lima <alberto.btlima@outlook.com>
2026-05-29 15:45:48 -0300

    add: componente Container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
+import { Container } from "@/components/Container";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <div>
+    <Container>
       <header>
         <h1 className="text-6xl font-bold text-center py-8">Aqui é a HEADER</h1>
+
+        <p className="text-justify p-8">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse fugiat
+          modi doloribus tempore ratione nulla dolores earum obcaecati,
+          repellendus error aut assumenda minus deleniti cupiditate minima
+          veniam officiis accusamus consequuntur! Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Esse fugiat modi doloribus tempore
+          ratione nulla dolores earum obcaecati, repellendus error aut assumenda
+          minus deleniti cupiditate minima veniam officiis accusamus
+          consequuntur! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Esse fugiat modi doloribus tempore ratione nulla dolores earum
+          obcaecati, repellendus error aut assumenda minus deleniti cupiditate
+          minima veniam officiis accusamus consequuntur!
+        </p>
       </header>
 
       <Suspense fallback={<SpinLoader />}>
@@ -16,6 +31,6 @@ export default async function HomePage() {
       <footer>
         <h1 className="text-6xl font-bold text-center py-8">Aqui é o FOOTER</h1>
       </footer>
-    </div>
+    </Container>
   );
 }
