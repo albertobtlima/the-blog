546414d2a680feea743ef10988ba9f4045aa1b7f
Alberto Lima <alberto.btlima@outlook.com>
2026-07-21 15:56:18 -0300

    iniciando botao delete post

--- src/components/PostsListAdmin/index.tsx
+++ src/components/PostsListAdmin/index.tsx
@@ -1,12 +1,43 @@
-import { findAllPostAdmin } from '@/lib/post/queries/admin';
+import { findAllPostAdmin } from "@/lib/post/queries/admin";
+import clsx from "clsx";
+import { Trash2Icon } from "lucide-react";
+import Link from "next/link";
 
 export default async function PostsListAdmin() {
   const posts = await findAllPostAdmin();
 
   return (
-    <div className='py-16'>
-      {posts.map(post => {
-        return <p key={post.id}>{post.title}</p>;
+    <div className="mb-16">
+      {posts.map((post) => {
+        return (
+          <div
+            className={clsx(
+              "p-2 flex gap-2 items-center justify-between",
+              !post.published && "bg-slate-300"
+            )}
+            key={post.id}
+          >
+            <Link href={`/admin/post/${post.id}`}>{post.title}</Link>
+
+            {!post.published && (
+              <span className="text-xs text-slate-600 italic">
+                (Não publicado)
+              </span>
+            )}
+
+            <button
+              className={clsx(
+                "text-red-500 cursor-pointer transition",
+                "[&_svg]:w-4 [&_svg]:h-4",
+                "hover:scale-120 hover:text-red-700"
+              )}
+              title="Apagar post"
+              aria-label={`Apagar post: ${post.title}`}
+            >
+              <Trash2Icon />
+            </button>
+          </div>
+        );
       })}
     </div>
   );
