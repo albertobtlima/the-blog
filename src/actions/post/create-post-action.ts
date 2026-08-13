74b25ed57d34079cde941f03c5bb4de2c160dc66
Alberto Lima <alberto.btlima@outlook.com>
2026-08-13 13:08:08 -0300

    add: metodos para criar, deletar e atualizar posts

--- src/actions/post/create-post-action.ts
+++ src/actions/post/create-post-action.ts
@@ -3,8 +3,11 @@
 import { makePartialPublicPost, PublicPost } from "@/dto/post/dto";
 import { PostCreateSchema } from "@/lib/post/validations";
 import { PostModel } from "@/models/post/post-model";
+import { postRepository } from "@/repositories/post";
 import { getZodErrorMessages } from "@/utils/get-zod-error-messages";
 import { makeSlugFromText } from "@/utils/make-slug-from-text";
+import { revalidateTag } from "next/cache";
+import { redirect } from "next/navigation";
 import { v4 as uuidV4 } from "uuid";
 
 type CreatePostActionState = {
@@ -45,8 +48,22 @@ export async function createPostAction(
     slug: makeSlugFromText(validPostData.title),
   };
 
-  return {
-    formState: newPost,
-    errors: [],
-  };
+  try {
+    await postRepository.create(newPost);
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return {
+        formState: newPost,
+        errors: [e.message],
+      };
+    }
+
+    return {
+      formState: newPost,
+      errors: ["Error desconhecido"],
+    };
+  }
+
+  revalidateTag("posts");
+  redirect(`/admin/post/${newPost.id}`);
 }
