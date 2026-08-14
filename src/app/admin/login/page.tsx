439518a369928c6d3ffb559742e361e3bd02cc6c
Alberto Lima <alberto.btlima@outlook.com>
2026-08-14 18:56:48 -0300

    add: pagina de login

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,5 +1,40 @@
-export const dynamic = 'force-dynamic';
+import { Button } from "@/components/Button";
+import { InputText } from "@/components/InputText";
+import clsx from "clsx";
+import { LogInIcon } from "lucide-react";
+
+export const dynamic = "force-dynamic";
 
 export default async function AdminLoginPage() {
-  return <div className='py-16 text-6xl'>AdminLoginPage</div>;
+  return (
+    <div
+      className={clsx(
+        "flex items-center justify-center",
+        "text-center max-w-sm mt-16 mb-32 mx-auto"
+      )}
+    >
+      <form action="" className="flex-1 flex flex-col gap-6">
+        <InputText
+          type="text"
+          name="username"
+          labelText="Usuário"
+          placeholder="Seu usuário"
+          disabled={false}
+        />
+
+        <InputText
+          type="password"
+          name="password"
+          labelText="Senha"
+          placeholder="Sua senha"
+          disabled={false}
+        />
+
+        <Button disabled={false} type="submit" className="mt-4">
+          <LogInIcon />
+          Entrar
+        </Button>
+      </form>
+    </div>
+  );
 }
