71156bb796cc04ea5cfb5dbf1f399c2e1aa3c5dd
Alberto Lima <alberto.btlima@outlook.com>
2026-08-14 19:21:46 -0300

    inicio da logica de login

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,40 +1,7 @@
-import { Button } from "@/components/Button";
-import { InputText } from "@/components/InputText";
-import clsx from "clsx";
-import { LogInIcon } from "lucide-react";
+import { LoginForm } from "@/components/admin/LoginForm";
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminLoginPage() {
-  return (
-    <div
-      className={clsx(
-        "flex items-center justify-center",
-        "text-center max-w-sm mt-16 mb-32 mx-auto"
-      )}
-    >
-      <form action="" className="flex-1 flex flex-col gap-6">
-        <InputText
-          type="text"
-          name="username"
-          labelText="Usuário"
-          placeholder="Seu usuário"
-          disabled={false}
-        />
-
-        <InputText
-          type="password"
-          name="password"
-          labelText="Senha"
-          placeholder="Sua senha"
-          disabled={false}
-        />
-
-        <Button disabled={false} type="submit" className="mt-4">
-          <LogInIcon />
-          Entrar
-        </Button>
-      </form>
-    </div>
-  );
+  return <LoginForm />;
 }
